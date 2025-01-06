7534d687de1f5a0fd7241e0e8844c9066b1f8830
tata-tdouble <tangetata@gmail.com>
2025-01-06 15:38:26 -0500

    Update UserOptions.kt
    hotfix: removed creds

--- src/main/kotlin/user/UserOptions.kt
+++ src/main/kotlin/user/UserOptions.kt
@@ -26,14 +26,14 @@ class UserOptions : Commands {
 
     fun loginFlowBegin() : Boolean {
 
-        mapOfOptions[AppConstants.APP_SETUP_BINANCE_API_KEY] = "pLW2Fh180QFWHCE41tTokNCTltHDNpbFuFhSDhJuMAiKLk1EHUAfd7PLwmRycGYh" //readFromConsole()
-        mapOfOptions[AppConstants.APP_SETUP_API_BINANCE_SECRET] = "aPeiV0oGCaeVLzvs2N5Oowvi8VKdCDp363Hm4wWu3znfWBDqSLR1vQqw2yF07RXJ" //readFromConsole()
-        mapOfOptions[AppConstants.APP_SETUP_TAAPI_API_KEY] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjY1MjE1NDlmNWFmOTRlZWNlODlkNGMzIiwiaWF0IjoxNzE3Njg3OTk1LCJleHAiOjMzMjIyMTUxOTk1fQ.EGf4h2c43gM_Wzdcg2neXmsTu2IyTeNjhl5ppNyqH04" //readFromConsole()
-        mapOfOptions[AppConstants.APP_SETUP_API_TAAPI_SECRET] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHVlIjoiNjY1MjE1NDlmNWFmOTRlZWNlODlkNGMzIiwiaWF0IjoxNzE3Njg3OTk1LCJleHAiOjMzMjIyMTUxOTk1fQ.EGf4h2c43gM_Wzdcg2neXmsTu2IyTeNjhl5ppNyqH04" //readFromConsole()
+        mapOfOptions[AppConstants.APP_SETUP_BINANCE_API_KEY] = "" //readFromConsole()
+        mapOfOptions[AppConstants.APP_SETUP_API_BINANCE_SECRET] = "" //readFromConsole()
+        mapOfOptions[AppConstants.APP_SETUP_TAAPI_API_KEY] = "" //readFromConsole()
+        mapOfOptions[AppConstants.APP_SETUP_API_TAAPI_SECRET] = "" //readFromConsole()
 
         state.updateOptions(mapOfOptions)
 
-        return mapOfOptions.size == 7
+        return mapOfOptions.size == 4
 
     }
 }
